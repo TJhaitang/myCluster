ce73ef1d92b13e3fc452eded02b5d743e022ac3e
tjhaitang <1319377413@qq.com>
2022-12-17 18:48:58 +0800

    1

--- R/myCluster.R
+++ R/myCluster.R
@@ -1,4 +1,29 @@
 hclust <- function(d,method="single",members=NULL){
     METHODS=c("single", "complete", "average", "mcquitty", "ward.D", "centroid", "median", "ward.D2")
     method=pmatch(method,METHODS)
+    if(is.null(members)){
+        stop("members must be specified")
+    }
+    if(method==-1){
+        stop("method must be one of ",paste(METHODS,collapse=", "))
+    }
+    #输入一个对称矩阵
+    tryCatch({
+        d=as.matrix(d)
+    },error=function(e){
+        stop("d must be a distance matrix")
+    })
+    if(!is.matrix(d)){
+        stop("d must be a distance matrix")
+    }
+
+    output=myCluster(d,method,members)
+    #output:
+    #output$merge1
+    #output$merge2
+    #output$height
+    #output$order--?
+
+    #这里需要再处理一下输出
+    return(output)
 }
--- src/myCluster.cpp
+++ src/myCluster.cpp
@@ -1,52 +1,155 @@
 // -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
 
-// we only include RcppArmadillo.h which pulls Rcpp.h in for us
 #include "RcppArmadillo.h"
-
-// via the depends attribute we tell Rcpp to create hooks for
-// RcppArmadillo so that the build process will know what to do
-//
 // [[Rcpp::depends(RcppArmadillo)]]
 
-// simple example of creating two matrices and
-// returning the result of an operatioon on them
-//
-// via the exports attribute we tell Rcpp to make this function
-// available from R
-//
-// [[Rcpp::export]]
-arma::mat rcpparma_hello_world() {
-    arma::mat m1 = arma::eye<arma::mat>(3, 3);
-    arma::mat m2 = arma::eye<arma::mat>(3, 3);
-	                     
-    return m1 + 3 * (m1 + m2);
+class clusterNode {
+public:
+    int idx1;
+    int idx2;
+    double dist;
+    clusterNode(int i1,int i2,double d):idx1(i1),idx2(i2),dist(d) {}
+};
+
+class clusterChain {
+public:
+    clusterNode *nodes;
+    int n;
+    int nNodes;
+    bool isOrdered;
+    clusterChain(int n):n(0),nNodes(n),isOrdered(false) {
+        nodes = new clusterNode[nNodes];
+    }
+    ~clusterChain() {
+        delete[] nodes;
+    }
+    void addNode(int i1,int i2,double d) {
+        nodes[n].idx1 = i1;
+        nodes[n].idx2 = i2;
+        nodes[n].dist = d;
+        n++;
+    }
+}
+
+class dubLinkList {//维护活跃结点序号
+private:
+    int * succ;
+    int * pred;
+public:
+    dubLinkList(int n) {
+        succ = new int[n];
+        pred = new int[n];
+        for(int i=0;i<n;i++) {
+            succ[i] = i+1;
+            pred[i] = i-1;
+        }
+        succ[n-1] = -1;
+        pred[0] = -1;
+    }
+    ~dubLinkList() {
+        delete[] idx;
+    }
+    int succ(int i) {
+        return succ[i];
+    }
+    int pred(int i) {
+        return pred[i];
+    }
+    void remove(int i) {
+        if(pred[i] != -1) {
+            succ[pred[i]] = succ[i];
+        }
+        if(succ[i] != -1) {
+            pred[succ[i]] = pred[i];
+        }
+    }
+};
+
+// 计算两个向量的距离
+
+// 使用最短距离法进行聚类
+//     The basis of this algorithm is an algorithm by Rohlf:
+//     F. James Rohlf, Hierarchical clustering using the minimum spanning tree,
+//     The Computer Journal, vol. 16, 1973, p. 93–95.
+void clusterMethod1(int n,arma::mat d,clusterChain *chain,int method=0,arma::vec members=NULL){
+    //建立双向链表存储未聚类的点
+    dubLinkList *list = new dubLinkList(n);
+    //建立一个数组存储每个点的最短距离
+    double *tempDist = new double[n];
+    //第一次迭代
+    int i1 = 0;
+    int i2 = 1;
+    double minDist = std::numeric_limits<double>::infinity();
+    for(int i=1;i<n;i++) {
+        tempDist[i] = d(i1,i);
+        if(tempDist[i] < minDist) {
+            minDist = tempDist[i];
+            i2 = i;
+        }
+    }
+    chain->addNode(i1,i2,minDist);
+    list->remove(i2);
+    //开始迭代
+    for(int i=2;i<n;i++) {
+        i1 = i2;
+        i2 = list->succ(0);
+        minDist = tempDist[i2];
+        for(int j=i2;j!=-1;j=list->succ(j)) {
+            if(d(i1,j) < tempDist[j]) {
+                tempDist[j] = d(i1,j);
+            }
+            if(tempDist[j] < minDist) {
+                minDist = tempDist[j];
+                i2 = j;
+            }
+        }
+        chain->addNode(i1,i2,minDist);
+        list->remove(i2);
+    }
+    delete[] tempDist;
+    delete list;
+    chain->isOrdered = false;
+}
+
+//根据合并顺序生成聚类结果
+void generateResult(int n,clusterChain *chain,arma::mat result) {
+    int *idx = new int[n];
+    for(int i=0;i<n;i++) {
+        idx[i] = i;
+    }
+    for(int i=0;i<n-1;i++) {
+        int i1 = chain->nodes[i].idx1;
+        int i2 = chain->nodes[i].idx2;
+        int i3 = n+i;
+        result(i,0) = i1;
+        result(i,1) = i2;
+        result(i,2) = chain->nodes[i].dist;
+        for(int j=0;j<n;j++) {
+            if(idx[j] == i2) {
+                idx[j] = i3;
+            }
+        }
+    }
+    delete[] idx;
 }
 
 
-// another simple example: outer product of a vector, 
-// returning a matrix
-//
+// 返回一个包含merge,height的mat
 // [[Rcpp::export]]
-arma::mat rcpparma_outerproduct(const arma::colvec & x) {
-    arma::mat m = x * x.t();
-    return m;
-}
-
-// and the inner product returns a scalar
-//
-// [[Rcpp::export]]
-double rcpparma_innerproduct(const arma::colvec & x) {
-    double v = arma::as_scalar(x.t() * x);
-    return v;
-}
-
-
-// and we can use Rcpp::List to return both at the same time
-//
-// [[Rcpp::export]]
-Rcpp::List rcpparma_bothproducts(const arma::colvec & x) {
-    arma::mat op = x * x.t();
-    double    ip = arma::as_scalar(x.t() * x);
-    return Rcpp::List::create(Rcpp::Named("outer")=op,
-                              Rcpp::Named("inner")=ip);
+arma::mat myCluster(arma::mat d,int method=0,arma:vec members=NULL) {
+    // 初始化
+    int n = d.n_rows;
+    clusterChain *chain = new clusterChain(n-1);
+    switch(method) {
+        case 0:
+            clusterMethod1(n,d,chain,method,members);
+            break;
+        default:
+            break;
+    }
+    // 生成结果
+    arma::mat result(n-1,3);
+    generateResult(n,chain,result);
+    delete chain;
+    return result;
 }
